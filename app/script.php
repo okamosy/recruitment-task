a0d287708c9df04dc98c1b5c0ea4374dd6862333
Seiji Okamoto <okamosy@gmail.com>
2017-02-15 21:32:12 +0000

    Logic for checking input file existence

--- app/script.php
+++ app/script.php
@@ -1,3 +1,23 @@
 <?php
 
-print "You must provide at least one parameter\n";
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Aura\Cli\CliFactory;
+
+$cliFactory = new CliFactory;
+$stdio = $cliFactory->newStdio();
+$context = $cliFactory->newContext($GLOBALS);
+
+$options = [];
+
+$getOptions = $context->getopt($options);
+
+if (($input = $getOptions->get(1)) == null) {
+    print "You must provide at least one parameter\n";
+    exit();
+}
+
+if( !file_exists($input)) {
+    print "The specified input file does not exist\n";
+    exit();
+}
